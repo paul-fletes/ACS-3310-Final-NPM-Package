0bb732fb105d1f90aab60b06c40505c24f4d52df
Paul Fletes <paul.fletes@gmail.com>
2023-10-04 12:54:59 -0500

    change arg to test workflow

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import { makeCzar } from "./makeCzar";
 import { generateRap } from "./rapGenerator";
 
 // Sample Useage
-const czarify = makeCzar('artform');
+const czarify = makeCzar('argonaut');
 const rap = generateRap('article');
 
-console.log(czarify) // "czartform"
+console.log(czarify) // "czargonaut"
 console.log(rap) // "I'm the czar, I goes hard; I be rhyming with a czarticle"
