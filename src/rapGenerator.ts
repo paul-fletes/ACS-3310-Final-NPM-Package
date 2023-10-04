e6d32e49a69a0dfbb1aad560273331e80b47624d
Paul Fletes <paul.fletes@gmail.com>
2023-10-04 11:21:38 -0500

    add rapGenerator function

---
+++ src/rapGenerator.ts
@@ -0,0 +1,9 @@
+import { makeCzar } from "./makeCzar";
+
+function generateRap(seedWord: string): string {
+  if (seedWord.startsWith('ar')) {
+    const rhymingWord: string = makeCzar(seedWord);
+    return `I'm the czar, I goes hard; I be rhyming with a ${rhymingWord} 🎤`
+  }
+  return `I'm the czar, I go far, but yo I can't cz-ar with ${seedWord} 🤬`
+}
